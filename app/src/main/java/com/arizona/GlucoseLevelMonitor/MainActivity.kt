670d4c9bcb6aa0c1afc2e0761d16d20c20e509d9
Nikhil Dronamraju <nikhil.dronamraju.0917@gmail.com>
2024-11-13 04:00:53 -0600

    UI one

--- app/src/main/java/com/arizona/GlucoseLevelMonitor/MainActivity.kt
+++ app/src/main/java/com/arizona/GlucoseLevelMonitor/MainActivity.kt
@@ -1,20 +1,34 @@
 package com.arizona.GlucoseLevelMonitor
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.arizona.GlucoseLevelMonitor.databinding.ActivityMainBinding
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        val date = Date().toString()
+        binding.saveDate.setText(date)
+        binding.clear.setOnClickListener{ view: View ->
+            binding.inputFasting.setText("")
+            binding.inputBreakfast.setText("")
+            binding.inputLunch.setText("")
+            binding.inputDinner.setText("")
         }
     }
 }
