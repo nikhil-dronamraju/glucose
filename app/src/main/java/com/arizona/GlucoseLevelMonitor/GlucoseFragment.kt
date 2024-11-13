fbd6a1c2de08df6e7d3ba2eab54a075c63e8e806
Nikhil Dronamraju <nikhil.dronamraju.0917@gmail.com>
2024-11-13 05:26:18 -0600

    Stable build

--- app/src/main/java/com/arizona/GlucoseLevelMonitor/GlucoseFragment.kt
+++ app/src/main/java/com/arizona/GlucoseLevelMonitor/GlucoseFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import java.util.*
 import com.arizona.GlucoseLevelMonitor.databinding.FragmentGlucoseBinding
 
 class GlucoseFragment : Fragment() {
@@ -24,6 +25,41 @@ class GlucoseFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-//        Nothing needs to happen here:
+        binding.apply{
+            saveDate.setText(Date().toString())
+            clear.setOnClickListener{ view: View ->
+                inputFasting.setText("")
+                inputBreakfast.setText("")
+                inputLunch.setText("")
+                inputDinner.setText("")
+                viewFastingFragment.setText("")
+                viewDateFragment.setText("")
+                viewLunchFragment.setText("")
+                viewBreakfastFragment.setText("")
+                viewDinnerFragment.setText("")
+            }
+            saveDate.setOnClickListener{ view: View ->
+                viewDateFragment.setText(Date().toString())
+                viewFastingFragment.setText(getGlucoseText(inputFasting.text.toString()))
+                viewBreakfastFragment.setText(getGlucoseText(inputBreakfast.text.toString()))
+                viewLunchFragment.setText(getGlucoseText(inputLunch.text.toString()))
+                viewDinnerFragment.setText(getGlucoseText(inputDinner.text.toString()))
+            }
+        }
+    }
+
+    private fun getGlucoseText( textVal: String ): String {
+        val glucoseNumber = textVal.toIntOrNull()
+        if ( glucoseNumber != null){
+            if (glucoseNumber < 70 ){
+                return "Hypoglycemic"
+            } else if (glucoseNumber <= 99){
+                return "Normal"
+            } else {
+                return "Abnormal"
+            }
+        } else {
+            return "Invalid val."
+        }
     }
 }
--- app/src/main/java/com/arizona/GlucoseLevelMonitor/MainActivity.kt
+++ app/src/main/java/com/arizona/GlucoseLevelMonitor/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.FragmentManager
 import com.arizona.GlucoseLevelMonitor.databinding.ActivityMainBinding
 import com.arizona.GlucoseLevelMonitor.databinding.FragmentGlucoseBinding
 import java.util.*
@@ -16,20 +17,11 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-        val date = Date().toString()
-        binding.saveDate.setText(date)
-        binding.clear.setOnClickListener{ view: View ->
-            binding.inputFasting.setText("")
-            binding.inputBreakfast.setText("")
-            binding.inputLunch.setText("")
-            binding.inputDinner.setText("")
         }
     }
 }
